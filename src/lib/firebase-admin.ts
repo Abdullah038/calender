12378288b19a9d04194a72a9544bb80064aaf265
WAYTOGO <abs55ca@gmail.com>
2025-06-22 21:08:24 -0400

    D

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,15 @@
 // lib/firebase-admin.ts
-import admin from 'firebase-admin';
+import admin from 'firebase-admin'
 
+// Parse the base64‐encoded JSON you stored in Vercel’s FIREBASE_SERVICE_ACCOUNT
 const serviceAccount = JSON.parse(
   Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT!, 'base64').toString('utf8')
-);
+)
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-  });
+  })
 }
 
-const db = admin.firestore();
-export { db };
+export const db = admin.firestore()
